6a2e69b08fc25ac5539b7b2d43c15073e0b70a46
soki <woosukplay3@gmail.com>
2022-02-20 00:12:00 +0900

    git initiated

---
+++ source/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::string alert_message;
+
+    alert_message = "** Project Initiated. **";
+
+    cout << alert_message << endl;
+
+    return 0;
+}
